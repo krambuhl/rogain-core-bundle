268281e619a70b56d7aa0c8d578ce9b19744e3b0
Evan Krambuhl <thekrambuhl@gmail.com>
2016-03-23 23:01:34 -0700

    try again

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 module.exports = {
-  components: require('./lib/components.js')
+  components: {
+    Children: require('rogain-component-children'),
+    Defined: require('rogain-component-defined'),
+    Each: require('rogain-component-each'),
+    Else: require('rogain-component-else'),
+    Empty: require('rogain-component-empty'),
+    Frame: require('rogain-component-frame'),
+    If: require('rogain-component-if'),
+    Range: require('rogain-component-range'),
+    Slot: require('rogain-component-slot'),
+    Unless: require('rogain-component-unless')
+  }
 };
