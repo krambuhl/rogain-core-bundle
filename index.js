3be22abfdba85fdde4a15ab52e87698b955938bb
Evan Krambuhl <thekrambuhl@gmail.com>
2016-04-10 18:41:51 -0700

    simplify

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
 module.exports = {
-  components: {
-    Children: require('rogain-component-children'),
-    Defined: require('rogain-component-defined'),
-    Each: require('rogain-component-each'),
-    Else: require('rogain-component-else'),
-    Empty: require('rogain-component-empty'),
-    Frame: require('rogain-component-frame'),
-    If: require('rogain-component-if'),
-    Range: require('rogain-component-range'),
-    Slot: require('rogain-component-slot'),
-    Unless: require('rogain-component-unless')
-  }
+  Each: require('rogain-component-each'),
+  Frame: require('rogain-component-frame'),
+  If: require('rogain-component-if')
 };
